fc0cd49fb1da9724294ac16d10a5ee62dea64eb7
greyewi1 <greyewi@gmail.com>
2021-04-09 16:34:42 +0500

    fixes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
-import './App.scss';
-import { connect } from 'react-redux'
+import './App.scss'
+import {connect} from 'react-redux'
 import {
   pageListSelector,
   setActivePage,
   activePageSelector,
 } from './modules/pages'
+import {useState, useCallback} from 'react'
 
 function App({
-  pageList
-}) {
-  
-  let idList
+               pageList
+             }) {
 
-  const getIds = (pageList) => {
+  const [idList, setIdList] = useState([])
+
+  const getIds = useCallback((pageList) => {
     const pages = pageList.entities.pages
     const result = []
     for (const page in pages) {
       result.push(pages[page].id)
-      // console.log(pages[page].id)
     }
-    idList = result
-    console.log(idList)
-    
-  }
+    return result
+  }, [pageList])
 
-  
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => getIds(pageList)}>Test - show all IDs</button>
+        <button onClick={() => setIdList(getIds(pageList))}>Det data</button>
         <div>{idList}</div>
       </header>
-      
+
     </div>
-  );
+  )
 }
 
 
